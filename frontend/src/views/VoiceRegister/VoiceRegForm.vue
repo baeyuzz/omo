795cdb8ec360915abc6b220622e8b1634c326aee
배유진 <seow0124@naver.com>
2020-09-17 14:38:37 +0900

    FE / register page

---
+++ frontend/src/views/VoiceRegister/VoiceRegForm.vue
@@ -0,0 +1,113 @@
+<template>
+    <div>
+        <Nav/>
+        <div class="regForm" style="margin-top : 5%;">
+            <h1>
+            음성등록
+            </h1>
+        </div>
+        <div style="text-align : center; color : black" class="regForm" >
+            <div style="margin-top : 2%; background-color : white; width : 480px; border-radius : 15px">
+
+                <div class="regForm">
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td><label>모임 코드</label></td>
+                                <td> <input class="regInput"  placeholder="ex) A123"/></td>
+                            </tr>
+                            <tr>
+                                <td><label>이름</label></td>
+                                <td> <input class="regInput"  placeholder="ex) 홍길동"/></td>
+                            </tr>
+                            <tr>
+                                <td><label>휴대 전화</label></td>
+                                <td><input class="regInput" type="tel" placeholder="ex) 010-1234-1234"/></td>
+                            </tr>
+                            <tr>
+                                <td><label>주소</label></td>
+                                <td><input class="regInput" placeholder="ex) 경기 안양"/></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="regForm regMent">
+                    다음 버튼을 누르시면 음성 인식을 위한 녹음이 시작됩니다
+                </div>
+                <div class="regForm">
+                    <button class="regBtn cancel">취소</button>
+                    <button class="regBtn">다음</button>
+                </div>
+            </div>
+        </div>
+  </div>
+</template>
+
+<script>
+import Nav from '@/components/Nav.vue'
+
+export default {
+  name: 'VoiceRegForm',
+  components: {
+    Nav
+  }
+}
+</script>
+<style scoped>
+.regForm {
+    width: 100%;
+    text-align: center;
+    display: flex;
+    justify-content: center;
+}
+.regInput {
+    background-color: #ffffff00;
+    font-size : 15px;
+    padding : 15px 20px 15px 20px;
+    margin: 5px;
+    /* border-radius: 10px; */
+    border : none;
+    outline: none;
+    width: 250px;
+    border-bottom: 1px solid;
+    color : black;
+}
+.regInput::placeholder {
+  color: #1624429d;
+  font-size : 15px;
+  font-weight: bold;
+}
+.regBtn {
+    padding : 5px 15px 5px 15px;
+    margin: 30px 5px 8% 5px;
+    border-radius: 5px;
+    border : none;
+    outline: none;
+    background-color: #005f7c7e;
+    color : black;
+    box-shadow: 0.5px 1px #292929;
+    text-align: center;
+    display: flex;
+    justify-content: center;
+    font-size: 15px;
+}
+table{
+    text-align: left;
+    margin-top: 5%;
+    border-spacing: 0 15px;
+}
+
+.inputGroup{
+    display: flex;
+    justify-content: center;
+}
+
+.regMent {
+    margin-top : 40px;
+    margin-bottom : 15px;
+}
+label {
+    font-size : 18px;
+    margin: 5px;
+}
+</style>
