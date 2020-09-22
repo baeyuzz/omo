e2c98f93ffc6748860affe080ef8fd8d3a3c5046
김강현 <bebekh1216@gmail.com>
2020-09-22 18:15:29 +0900

    update : camera page layout (incomplete)

--- frontend/src/views/CreateList/WebCam.vue
+++ frontend/src/views/CreateList/WebCam.vue
@@ -1,8 +1,13 @@
 <template>
-    <div id="webcam">
+    <div id="webcam"
+    tabindex="1"
+    @keyup="goBack(this.event)"
+    >
         <div><video ref="video" id="video" width="640" height="480" autoplay></video></div>
-        <div><button id="snap" v-on:click="capture()">사진 촬영</button></div>
-        <router-link to="/main" style="text-decoration:none; color : white;">취소 </router-link>
+        <div>
+        <button id="snap" v-on:click="capture()"><img src="@/assets/camera.png" width="50px" height="50px;" /></button>
+        <router-link to="/main" style="text-decoration:none; color : white;"><img src="@/assets/close.png" width="50px" height="50px;" /></router-link>
+        </div>
         <canvas ref="canvas" id="canvas" width="640" height="480"></canvas>
         <ul>
             <li v-for="c in captures" :key="c">
@@ -32,8 +37,16 @@
                     this.video.play();
                 });
             }
+            this.focus
         },
         methods: {
+            doSomething(){
+                console.log("asdf");
+            },
+            goBack(event){
+                console.log(event);
+                // this.$router.push("/main");
+            },
             capture() {
                 this.canvas = this.$refs.canvas;
                 var context = this.canvas.getContext("2d").drawImage(this.video, 0, 0, 640, 480);
@@ -63,7 +76,8 @@
     #webcam {
         text-align: center;
         color: #2c3e50;
-        margin-top: 60px;
+        /* margin-top: 60px; */
+        /* height: 100vh; */
     }
     #video {
         background-color: #000000;
@@ -74,5 +88,9 @@
     li {
         display: inline;
         padding: 5px;
+    }
+    button{
+        background: none;
+        border: none;
     }
 </style>
