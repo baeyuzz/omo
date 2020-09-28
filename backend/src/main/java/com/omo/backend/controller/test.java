d6d1c1e911f088beac780c03cf144278f79b785b
배유진 <seow0124@naver.com>
2020-09-28 17:58:33 +0900

    add comment

--- backend/src/main/java/com/omo/backend/controller/test.java
+++ backend/src/main/java/com/omo/backend/controller/test.java
@@ -37,15 +37,20 @@ public class test {
 
         String code = (String) info.get("code");
 
+        code = "9조"; // 임의로 해놓음
+
+
         String fileName= file.getOriginalFilename();
-        File f= new File("C:\\ssafy\\project2\\pjt3\\s03p23a509\\AI\\Voice\\ssafy\\" + fileName); // 나중엔 ssafy 대신 code 넣어야함
+        File f= new File("C:\\ssafy\\project2\\pjt3\\s03p23a509\\AI\\Voice\\" + code + "\\" + fileName); 
         file.transferTo(f);    
 
         System.out.println("Python Call");
         String[] command = new String[3];
         command[0] = "python";
         command[1] = "C:\\ssafy\\project2\\pjt3\\s03p23a509\\AI\\Voice\\test.py";
-        command[2] = "ssafy"; // 여기도 ssafy 대신 code(기관명) 넣어야함
+        command[2] = code; 
+
+        // python code 실행하는 부분
         try {
             execPython(command,0);
         } catch (Exception e) {
@@ -80,10 +85,11 @@ public class test {
             System.out.println("폴더 생성 실패");
         }
 
-
+        // .wav 파일 로컬에 저장
         File f= new File(path + "\\record.wav");
         file.transferTo(f);   
 
+        // python 실행 부분
         System.out.println("Python Call");
         String[] command = new String[4];
         command[0] = "python";
@@ -98,7 +104,7 @@ public class test {
             execPython(command,1);
         } // mfcc를 .npy 로 저장
 
-        
+
         // 여기는 training 하는 코드인데 굳이 java에서 안하고 python에서 하면 될 둣
         // System.out.println("Python Call 2");
         // String[] command = new String[4];
